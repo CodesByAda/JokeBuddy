61a16cf727fabc3db11534ed4baa8329bff83bfd
CodesByAda <adarshantony223@gmail.com>
2024-09-20 21:15:47 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs');
-const  path = require('path');
+const path = require('path');
 const commandHandler = require('./handlers/commandHandler');
 const eventHandler = require('./handlers/eventHandler');
 const slashCommandHandler = require('./handlers/slashCommandHandler');
 
-const client = new Client({ 
-	intents: [
-		GatewayIntentBits.Guilds,
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMessageTyping,
         GatewayIntentBits.GuildPresences,
         GatewayIntentBits.GuildMembers
-	] 
+    ]
 });
 
 commandHandler(client);
